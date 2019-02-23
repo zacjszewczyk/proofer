c9b71f59b19e98000c8b504f2ddee5a6e26843f5
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-02-23 17:52:03 -0500

    I think I'm done.

--- Marked.py
+++ Marked.py
@@ -64,6 +64,133 @@ def SyllableCount(word):
         count += 1
     return count
 
+def sylco(word) :
+
+    word = word.lower()
+
+    # exception_add are words that need extra syllables
+    # exception_del are words that need less syllables
+
+    exception_add = ['serious','crucial']
+    exception_del = ['fortunately','unfortunately']
+
+    co_one = ['cool','coach','coat','coal','count','coin','coarse','coup','coif','cook','coign','coiffe','coof','court']
+    co_two = ['coapt','coed','coinci']
+
+    pre_one = ['preach']
+
+    syls = 0 #added syllable number
+    disc = 0 #discarded syllable number
+
+    #1) if letters < 3 : return 1
+    if len(word) <= 3 :
+        syls = 1
+        return syls
+
+    #2) if doesn't end with "ted" or "tes" or "ses" or "ied" or "ies", discard "es" and "ed" at the end.
+    # if it has only 1 vowel or 1 set of consecutive vowels, discard. (like "speed", "fled" etc.)
+
+    if word[-2:] == "es" or word[-2:] == "ed" :
+        doubleAndtripple_1 = len(re.findall(r'[eaoui][eaoui]',word))
+        if doubleAndtripple_1 > 1 or len(re.findall(r'[eaoui][^eaoui]',word)) > 1 :
+            if word[-3:] == "ted" or word[-3:] == "tes" or word[-3:] == "ses" or word[-3:] == "ied" or word[-3:] == "ies" :
+                pass
+            else :
+                disc+=1
+
+    #3) discard trailing "e", except where ending is "le"  
+
+    le_except = ['whole','mobile','pole','male','female','hale','pale','tale','sale','aisle','whale','while']
+
+    if word[-1:] == "e" :
+        if word[-2:] == "le" and word not in le_except :
+            pass
+
+        else :
+            disc+=1
+
+    #4) check if consecutive vowels exists, triplets or pairs, count them as one.
+
+    doubleAndtripple = len(re.findall(r'[eaoui][eaoui]',word))
+    tripple = len(re.findall(r'[eaoui][eaoui][eaoui]',word))
+    disc+=doubleAndtripple + tripple
+
+    #5) count remaining vowels in word.
+    numVowels = len(re.findall(r'[eaoui]',word))
+
+    #6) add one if starts with "mc"
+    if word[:2] == "mc" :
+        syls+=1
+
+    #7) add one if ends with "y" but is not surrouned by vowel
+    if word[-1:] == "y" and word[-2] not in "aeoui" :
+        syls +=1
+
+    #8) add one if "y" is surrounded by non-vowels and is not in the last word.
+
+    for i,j in enumerate(word) :
+        if j == "y" :
+            if (i != 0) and (i != len(word)-1) :
+                if word[i-1] not in "aeoui" and word[i+1] not in "aeoui" :
+                    syls+=1
+
+    #9) if starts with "tri-" or "bi-" and is followed by a vowel, add one.
+
+    if word[:3] == "tri" and word[3] in "aeoui" :
+        syls+=1
+
+    if word[:2] == "bi" and word[2] in "aeoui" :
+        syls+=1
+
+    #10) if ends with "-ian", should be counted as two syllables, except for "-tian" and "-cian"
+
+    if word[-3:] == "ian" : 
+    #and (word[-4:] != "cian" or word[-4:] != "tian") :
+        if word[-4:] == "cian" or word[-4:] == "tian" :
+            pass
+        else :
+            syls+=1
+
+    #11) if starts with "co-" and is followed by a vowel, check if exists in the double syllable dictionary, if not, check if in single dictionary and act accordingly.
+
+    if word[:2] == "co" and word[2] in 'eaoui' :
+
+        if word[:4] in co_two or word[:5] in co_two or word[:6] in co_two :
+            syls+=1
+        elif word[:4] in co_one or word[:5] in co_one or word[:6] in co_one :
+            pass
+        else :
+            syls+=1
+
+    #12) if starts with "pre-" and is followed by a vowel, check if exists in the double syllable dictionary, if not, check if in single dictionary and act accordingly.
+
+    if word[:3] == "pre" and word[3] in 'eaoui' :
+        if word[:6] in pre_one :
+            pass
+        else :
+            syls+=1
+
+    #13) check for "-n't" and cross match with dictionary to add syllable.
+
+    negative = ["doesn't", "isn't", "shouldn't", "couldn't","wouldn't"]
+
+    if word[-3:] == "n't" :
+        if word in negative :
+            syls+=1
+        else :
+            pass   
+
+    #14) Handling the exceptional words.
+
+    if word in exception_del :
+        disc+=1
+
+    if word in exception_add :
+        syls+=1     
+
+    # calculate the output
+    return numVowels - disc + syls
+
 # Global variables for pasring Markdown
 types = ["", "", ""]
 active = ""
@@ -414,12 +541,10 @@ def GenFile(iname):
             # the number of syllables in the remaining word is >= 3, found a complex word.
             if (not (re.search("^[A-Z]", stripped))):
                 if ("-" not in stripped):
-                    if (SyllableCount(re.sub("(es|ed|ing)$", "",stripped.lower())) >= 3):
+                    if (sylco(stripped.lower()) >= 3):
                         complex_words += 1
 
-            # print stripped
-            # print SyllableCount(re.sub("(es|ed|ing)$","",stripped.lower()))
-            syllable_count += SyllableCount(re.sub("(es|ed|ing)$","",stripped.lower()))
+            syllable_count += sylco(stripped.lower())
 
         word_count.append(wc)
 
@@ -452,7 +577,22 @@ def GenFile(iname):
 
     # Calculate Flesch-Kincaid Readability Test
     # higher scores indicate material that is easier to read; lower numbers indicate difficulty.
-    fkr = 206.835 - (1.015*(float(fk_wc)/float(total_sentences)) + 84.6*(float(syllable_count)/float(fk_wc)))
+    fkr = 206.835 - 1.015*(float(fk_wc)/float(total_sentences)) - 84.6*(float(syllable_count)/float(fk_wc))
+
+    if (fkr <= 30.0):
+        fkr = "<span class='extreme'>%3.2f</span>" % (fkr)
+    if (fkr <= 50.0):
+        fkr = "<span class='hard'>%3.2f</span>" % (fkr)
+    if (fkr <= 60.0):
+        fkr = "<span class='tough'>%3.2f</span>" % (fkr)
+    if (fkr <= 70.0):
+        fkr = "<span class='plain'>%3.2f</span>" % (fkr)
+    if (fkr <= 80.0):
+        fkr = "<span class='fair'>%3.2f</span>" % (fkr)
+    if (fkr <= 90.0):
+        fkr = "<span class='easy'>%3.2f</span>" % (fkr)
+    if (fkr <= 100.00):
+        fkr = "<span class='simple'>%3.2f</span>" % (fkr)
 
     # Calculate the Flesch-Kincaid Grade level:
     # the number of years of education generally required to understand this text.
