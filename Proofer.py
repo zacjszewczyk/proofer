a9e758a9fbe41374d82ceda332dd3cfddbc016e8
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-09-05 12:15:45 -0400

    Work

--- Proofer.py
+++ Proofer.py
@@ -6,6 +6,7 @@ import os
 from time import sleep
 import datetime
 import re
+from re import findall
 sys.path.insert(0, '/Users/zjszewczyk/Dropbox/Code/Standalone')
 from Markdown import Markdown
 
@@ -29,59 +30,39 @@ exclude         = be_verbs+["the", "a", "or", "my", "and", "to", "we", "I", "for
 # Purpose: Accept a word and return the number of syllables
 # Parameters: word: Word to be parsed. (String)
 # H/t: https://github.com/eaydin/sylco
-def SyllableCount(word) :
-
+def SyllableCount(word):
     word = word.lower()
 
-    # exception_add are words that need extra syllables
-    # exception_del are words that need less syllables
+    syls = 0 # Number of added syllables
+    disc = 0 # Number of discarded syllables
 
-    exception_add = ['serious','crucial']
-    exception_del = ['fortunately','unfortunately']
-
-    co_one = ['cool','coach','coat','coal','count','coin','coarse','coup','coif','cook','coign','coiffe','coof','court']
-    co_two = ['coapt','coed','coinci']
-
-    pre_one = ['preach']
-
-    syls = 0 #added syllable number
-    disc = 0 #discarded syllable number
-
-    #1) if letters < 3 : return 1
+    #1) If three or less letters, one syllable
     if len(word) <= 3 :
-        syls = 1
-        return syls
+        return 1
 
-    #2) if doesn't end with "ted" or "tes" or "ses" or "ied" or "ies", discard "es" and "ed" at the end.
+    #2) If doesn't end with "ted" or "tes" or "ses" or "ied" or "ies", discard "es" and "ed" at the end.
     # if it has only 1 vowel or 1 set of consecutive vowels, discard. (like "speed", "fled" etc.)
+    double_vowel = len(findall(r'[eaoui][eaoui]',word))
 
-    if word[-2:] == "es" or word[-2:] == "ed" :
-        doubleAndtripple_1 = len(re.findall(r'[eaoui][eaoui]',word))
-        if doubleAndtripple_1 > 1 or len(re.findall(r'[eaoui][^eaoui]',word)) > 1 :
-            if word[-3:] == "ted" or word[-3:] == "tes" or word[-3:] == "ses" or word[-3:] == "ied" or word[-3:] == "ies" :
+    if word[-2:] in ["es", "ed"]:
+        if double_vowel > 1 or len(findall(r'[eaoui][^eaoui]',word)) > 1 :
+            if word[-3:] in ["ted", "tes", "ses", "ied", "ies"]:
                 pass
-            else :
+            else:
                 disc+=1
 
     #3) discard trailing "e", except where ending is "le"  
-
-    le_except = ['whole','mobile','pole','male','female','hale','pale','tale','sale','aisle','whale','while']
-
     if word[-1:] == "e" :
-        if word[-2:] == "le" and word not in le_except :
+        if word[-2:] == "le" and word not in ['whole','mobile','pole','male','female','hale','pale','tale','sale','aisle','whale','while']:
             pass
-
         else :
             disc+=1
 
     #4) check if consecutive vowels exists, triplets or pairs, count them as one.
-
-    doubleAndtripple = len(re.findall(r'[eaoui][eaoui]',word))
-    tripple = len(re.findall(r'[eaoui][eaoui][eaoui]',word))
-    disc+=doubleAndtripple + tripple
+    disc += double_vowel + len(findall(r'[eaoui][eaoui][eaoui]',word))
 
     #5) count remaining vowels in word.
-    numVowels = len(re.findall(r'[eaoui]',word))
+    numVowels = len(findall(r'[eaoui]',word))
 
     #6) add one if starts with "mc"
     if word[:2] == "mc" :
@@ -92,7 +73,6 @@ def SyllableCount(word) :
         syls +=1
 
     #8) add one if "y" is surrounded by non-vowels and is not in the last word.
-
     for i,j in enumerate(word) :
         if j == "y" :
             if (i != 0) and (i != len(word)-1) :
@@ -100,7 +80,6 @@ def SyllableCount(word) :
                     syls+=1
 
     #9) if starts with "tri-" or "bi-" and is followed by a vowel, add one.
-
     if word[:3] == "tri" and word[3] in "aeoui" :
         syls+=1
 
@@ -108,49 +87,41 @@ def SyllableCount(word) :
         syls+=1
 
     #10) if ends with "-ian", should be counted as two syllables, except for "-tian" and "-cian"
-
     if word[-3:] == "ian" : 
-    #and (word[-4:] != "cian" or word[-4:] != "tian") :
-        if word[-4:] == "cian" or word[-4:] == "tian" :
+        if word[-4:] in ["cian", "tian"]:
             pass
         else :
             syls+=1
 
     #11) if starts with "co-" and is followed by a vowel, check if exists in the double syllable dictionary, if not, check if in single dictionary and act accordingly.
-
     if word[:2] == "co" and word[2] in 'eaoui' :
-
-        if word[:4] in co_two or word[:5] in co_two or word[:6] in co_two :
+        if (set(['coapt','coed','coinci']).intersection([word[:4], word[:5], word[:6]])):
             syls+=1
-        elif word[:4] in co_one or word[:5] in co_one or word[:6] in co_one :
+        elif (set(['cool','coach','coat','coal','count','coin','coarse','coup','coif','cook','coign','coiffe','coof','court']).intersection([word[:4], word[:5], word[:6]])):
             pass
         else :
             syls+=1
 
     #12) if starts with "pre-" and is followed by a vowel, check if exists in the double syllable dictionary, if not, check if in single dictionary and act accordingly.
-
     if word[:3] == "pre" and word[3] in 'eaoui' :
-        if word[:6] in pre_one :
+        if word[:6] in ['preach']:
             pass
         else :
             syls+=1
 
     #13) check for "-n't" and cross match with dictionary to add syllable.
-
-    negative = ["doesn't", "isn't", "shouldn't", "couldn't","wouldn't"]
-
     if word[-3:] == "n't" :
-        if word in negative :
+        if word in ["doesn't", "isn't", "shouldn't", "couldn't","wouldn't"]:
             syls+=1
-        else :
-            pass   
+        else:
+            pass
 
     #14) Handling the exceptional words.
-
-    if word in exception_del :
+    # exception_del are words that need less syllables
+    if word in ['fortunately','unfortunately']:
         disc+=1
-
-    if word in exception_add :
+    # exception_add are words that need extra syllables
+    if word in ['serious','crucial']:
         syls+=1     
 
     # calculate the output
